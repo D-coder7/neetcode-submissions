c622db79fd1410d679daa25f08f222e7e8c38070
D-coder7 <95760005+D-coder7@users.noreply.github.com>
2026-06-15 18:11:41 +0530

    Remove: max-water-container - submission-0

--- Data Structures & Algorithms/max-water-container/submission-0.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        maxAr = 0
-        for i in range(len(heights)):
-            for j in range(i+1, len(heights)):
-                maxAr = max(maxAr, (j-i)*min(heights[i], heights[j]))
-        return maxAr
